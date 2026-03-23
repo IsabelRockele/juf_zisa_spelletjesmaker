e4c7b8302333cecd38af9cb7fbbf86023aaf76e6
IsabelRockele <rockele@msn.com>
2026-03-23 08:38:25 +0100

    Delete bundel_store.js

--- bundel/bundel_store.js
+++
@@ -1,5 +0,0 @@
-window.bundelPush = function(item) {
-  const bundel = JSON.parse(localStorage.getItem('werkbladBundel') || '[]');
-  bundel.push(item);
-  localStorage.setItem('werkbladBundel', JSON.stringify(bundel));
-};
