e63e57af048c0f4d81bcfaf690b44d027be16b2f
IsabelRockele <rockele@msn.com>
2026-04-09 18:08:42 +0200

    Update opvolging_huistaken-app.js

--- pro/opvolging_huistaken/js/opvolging_huistaken-app.js
+++ pro/opvolging_huistaken/js/opvolging_huistaken-app.js
@@ -1,3 +1,4 @@
+import { initializeApp, getApps, getApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import {
   getFirestore,
