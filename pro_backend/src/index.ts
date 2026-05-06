eb7d66ad56cbc2daaffaa611c93b8fda8a750bad
IsabelRockele <rockele@msn.com>
2026-05-06 21:24:39 +0200

    Update index.ts

--- pro_backend/src/index.ts
+++ pro_backend/src/index.ts
@@ -39,6 +39,7 @@ const LICENSE_COLLECTIONS  = ["licenses", "Licenties"];
 
 const ALLOWED_ORIGINS = [
   "https://isabelrockele.github.io",
+  "https://tools.jufzisa.be",      // ← enige nieuwe regel
   "http://localhost:5500",
   "http://127.0.0.1:5500",
   "http://localhost:5173",
