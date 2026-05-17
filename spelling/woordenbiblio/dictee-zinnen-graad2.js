a88583da7b9d5e68cecdc81ff938ed75e197f0ec
IsabelRockele <rockele@msn.com>
2026-05-17 19:41:48 +0200

    Create dictee-zinnen-graad2.js

---
+++ spelling/woordenbiblio/dictee-zinnen-graad2.js
@@ -0,0 +1,109 @@
+/* ==========================================================
+   dictee-zinnen-graad2.js
+   Voorbeeldzinnen voor het weekdictee — graad 2 (L3 + L4)
+   
+   Format per regel:
+     "<groep>|<grondvorm>": "Voorbeeldzin met het woord erin."
+   
+   - <groep>     = exact dezelfde key als in graad2.js (zonder -g2 suffix)
+   - <grondvorm> = de "tekst" van het woord (bij meervouden: enkelvoud,
+                   bij verkleinwoorden: grondwoord, bij werkwoorden: infinitief)
+   
+   Zinnen graad 2 mogen iets complexer zijn dan graad 1:
+   - Wat langere zinnen (8-12 woorden)
+   - Subwoord nog steeds bekend
+   - Mag zinsbouw bevatten die past bij L3/L4 (samengestelde zinnen, etc.)
+   ========================================================== */
+window.SpellingDicteeZinnen = window.SpellingDicteeZinnen || {};
+
+window.SpellingDicteeZinnen.graad2 = {
+
+  // ==========================================================
+  // HOORWOORDEN
+  // ==========================================================
+
+  // ----- ng / nk woorden -----
+  // TODO
+
+  // ----- sch-woorden -----
+  // TODO
+
+  // ----- Clusters (meerdere medeklinkers in midden) -----
+  // TODO
+
+  // ----- ei / ij woorden -----
+  // TODO
+
+  // ----- au / ou woorden -----
+  // TODO
+
+  // ----- aai / ooi / oei / eeuw / ieuw / uw -----
+  // TODO
+
+  // ----- ch / cht / gt -----
+  // TODO
+
+  // ----- Doffe klank -----
+  // TODO
+
+
+  // ==========================================================
+  // REGELWOORDEN
+  // ==========================================================
+
+  // ----- Verdubbel / verenkel -----
+  // TODO
+
+  // ----- Verlengingsregel t/d + p/b -----
+  // TODO
+
+  // ----- Verkleinwoorden (-je, -tje, -pje, -etje, -kje) -----
+  // TODO
+
+  // ----- Meervouden (-en, -s, -'s, -eren) -----
+  // TODO
+
+  // ----- Doffe klank in voorvoegsel -----
+  // TODO
+
+  // ----- Doffe klank in achtervoegsel -----
+  // TODO
+
+
+  // ==========================================================
+  // WERKWOORDEN
+  // ==========================================================
+
+  // ----- OTT (onvoltooid tegenwoordige tijd) -----
+  // TODO
+
+  // ----- VTT (voltooid tegenwoordige tijd) -----
+  // TODO
+
+  // ----- OVT zwak -----
+  // TODO
+
+  // ----- OVT sterk -----
+  // TODO
+
+  // ----- VVT (voltooid verleden tijd) -----
+  // TODO
+
+
+  // ==========================================================
+  // HOOFDLETTERS
+  // ==========================================================
+  // TODO
+
+
+  // ==========================================================
+  // L4-SPECIFIEK
+  // ==========================================================
+
+  // ----- -teit / -heid -----
+  // TODO
+
+  // ----- Leenwoorden -----
+  // TODO
+
+};
