048b68eb668ee2e2b1ba2fcb146a37c16dc62c2a
IsabelRockele <rockele@msn.com>
2026-05-20 16:58:57 +0200

    Create kleuren.js

---
+++ planbord/kleuren.js
@@ -0,0 +1,14 @@
+// === KLEUREN ===
+// Beschikbare kleurfamilies voor vakken
+
+const KLEUREN = ['paars', 'roze', 'groen', 'teal', 'blauw', 'geel', 'oranje', 'grijs'];
+
+// Standaardkleur per vak-type bij toevoegen
+const STANDAARD_KLEUR = {
+  vrij: 'paars',
+  checklist: 'teal',
+  timer: 'groen',
+  verjaardag: 'roze',
+  dagprogramma: 'blauw',
+  mededeling: 'geel',
+};
